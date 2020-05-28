36af2ec7e0afcfe980b0bdac06872e7ec8e2ddfc
Alexandre Akira <alexandreakao@edu.unifor.br>
2020-05-28 15:38:48 -0300

    backend

---
+++ backend/src/test/java/br/com/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
